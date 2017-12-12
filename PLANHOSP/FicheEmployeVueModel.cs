1a42579a1552a2276ed2960330579360fe9e1376
loloMass <amoin.massamba@gmail.com>
2017-12-12 21:00:47 +0100

    first commit

---
+++ PLANHOSP/FicheEmployeVueModel.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace PLANHOSP
+{
+    class FicheEmployeVueModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged([CallerMemberName]string str = "")
+        {
+            if (PropertyChanged!=null) // Vérifier qu'il ya au moins quelqu'un abonné à l'évent property changed
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(str));
+            }
+        }
+
+        private ObservableCollection<HospiEmployees> fichesEmploye;
+
+        public ObservableCollection<HospiEmployees> FichesEmploye { get => fichesEmploye;
+            set { 
+                if (value!=fichesEmploye)
+                {
+                    fichesEmploye = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        private HospiEmployees ficheEmployeSelectionnee;
+
+        public HospiEmployees FicheEmployeSelectionnee
+        {
+            get => ficheEmployeSelectionnee;
+            set
+            {
+                if (value != ficheEmployeSelectionnee)
+                {
+                    ficheEmployeSelectionnee = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public FicheEmployeVueModel()
+        {
+            FichesEmploye = new ObservableCollection<HospiEmployees>();
+            FicheEmployeSelectionnee = new HospiEmployees()
+            {
+                Nom = "Massamba",
+                Prenom = "Lorraine",
+                Age = 23,
+                PosteOccupe = "Directrice",
+                DateDeNaissance = DateTime.Now
+
+            };
+            FichesEmploye.Add(FicheEmployeSelectionnee);
+        }
+        
+        public ICommand AjoutFicheEmploye {
+            get {
+                if (ajoutFicheEmploye == null)
+                {
+                    ajoutFicheEmploye = new RelayCommand<Object>((obj) => FichesEmploye.Add(new HospiEmployees ()));
+                }
+                return ajoutFicheEmploye;
+            }
+        }
+
+        public ICommand RetraitFicheClient {
+            get
+            {
+                if (retraitFicheEmploye == null)
+                {
+                    retraitFicheEmploye = new RelayCommand<HospiEmployees>((employe) => FichesEmploye.Remove(employe));
+                }
+                return retraitFicheEmploye;
+            }
+        }
+
+        public ICommand EditionFicheEmploye {
+            get {
+                if (editionFicheEmploye == null)
+                {
+                    editionFicheEmploye = new RelayCommand<HospiEmployees>((employe) => ficheEmployeSelectionnee = employe);
+                }
+                return editionFicheEmploye;
+            }
+        }
+
+        private ICommand ajoutFicheEmploye;
+
+        private ICommand retraitFicheEmploye;
+
+        private ICommand editionFicheEmploye;
+        
+    }
+}
---
+++ PLANHOSP/HospiEmployees.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PLANHOSP
+{
+    public partial class HospiEmployees: INotifyPropertyChanged
+    {
+        private String nom;
+        private String prenom;
+        private DateTime dateDeNaissance;
+        private DateTime dateDentree;
+        private int age;
+        private String posteOccupe;
+
+        public string Nom { get => nom; set { if (value != nom) { nom = value; NotifyPropertyChanged(); } } }
+        public string Prenom { get => prenom; set { if (value != prenom) { prenom = value; NotifyPropertyChanged(); } } }
+        public DateTime DateDeNaissance { get => dateDeNaissance; set { if (value != dateDeNaissance) { dateDeNaissance = value; NotifyPropertyChanged(); } } }
+        public int Age { get => age; set { if (value != age) { age = value; NotifyPropertyChanged(); } } }
+        public string PosteOccupe { get => posteOccupe; set { if (value != posteOccupe) { posteOccupe = value; NotifyPropertyChanged(); } } }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged([CallerMemberName] string str = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(str));
+            }
+        }
+
+
+    }
+}
